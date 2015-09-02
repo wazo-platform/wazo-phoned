2b2db630634091c15f11d5d14712055e67848756
François Blackburn <fblackburn@avencall.com>
2015-09-02 08:41:11 -0400

    add unittests coverage

---
+++ xivo_dird_phoned/tests/test_controller.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+from mock import patch, sentinel as s
+from unittest import TestCase
+
+from xivo_dird_phoned.controller import Controller
+
+
+class TestController(TestCase):
+
+    def setUp(self):
+        self.rest_api = patch('xivo_dird_phoned.controller.RestApi').start().return_value
+        self.http = patch('xivo_dird_phoned.controller.DirectoriesConfiguration').start()
+
+    def tearDown(self):
+        patch.stopall()
+
+    def test_run_starts_rest_api(self):
+        config = self._create_config(**{
+            'rest_api': {'listen': '0.0.0.0', 'port_http': '9498', 'port_https': '9499'},
+            'debug': s.debug,
+        })
+        controller = Controller(config)
+        controller.run()
+        self.rest_api.run.assert_called_once_with()
+
+    def test_init_loads_sources(self):
+        config = self._create_config()
+
+        Controller(config)
+
+        self.http.assert_called_once_with(config['dird'])
+
+    def _create_config(self, **kwargs):
+        config = dict(kwargs)
+        config.setdefault('auth', {})
+        config.setdefault('dird', {})
+        config['dird'].setdefault('host', '')
+        config['dird'].setdefault('port', '')
+        config['dird'].setdefault('default_profile', '')
+        config.setdefault('rest_api', {})
+        config['rest_api'].setdefault('subnets_authorized', [])
+        return config
---
+++ xivo_dird_phoned/tests/test_http.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from hamcrest import assert_that, equal_to
+from unittest import TestCase
+
+from xivo_dird_phoned import http
+
+
+class TestHTTP(TestCase):
+
+    def test_that_find_vendor_by_user_agent_return_vendor_cisco(self):
+        expected_result = 'cisco'
+        user_agent_1 = 'xxx-allegro-alice-12356'
+        user_agent_2 = 'xxx-cisco-alice-12356'
+        result_1 = http.find_vendor_by_user_agent(user_agent_1)
+        result_2 = http.find_vendor_by_user_agent(user_agent_2)
+
+        assert_that(result_1, equal_to(expected_result))
+        assert_that(result_2, equal_to(expected_result))
+
+    def test_that_find_vendor_by_user_agent_return_vendor_aastra(self):
+        expected_result = 'aastra'
+        user_agent = 'xxx-aastra-alice-12356'
+        result = http.find_vendor_by_user_agent(user_agent)
+
+        assert_that(result, equal_to(expected_result))
+
+    def test_that_find_vendor_by_user_agent_return_vendor_snom(self):
+        expected_result = 'snom'
+        user_agent = 'xxx-snom-alice-12356'
+        result = http.find_vendor_by_user_agent(user_agent)
+
+        assert_that(result, equal_to(expected_result))
+
+    def test_that_find_vendor_by_user_agent_return_vendor_thomson(self):
+        expected_result = 'thomson'
+        user_agent = 'xxx-thomson-alice-12356'
+        result = http.find_vendor_by_user_agent(user_agent)
+
+        assert_that(result, equal_to(expected_result))
+
+    def test_that_find_vendor_by_user_agent_return_vendor_yealink(self):
+        expected_result = 'yealink'
+        user_agent = 'xxx-yealink-alice-12356'
+        result = http.find_vendor_by_user_agent(user_agent)
+
+        assert_that(result, equal_to(expected_result))
+
+    def test_that_find_vendor_by_user_agent_return_none_when_no_supported_user_agent(self):
+        expected_result = None
+        user_agent = 'xxx-not_supported-alice-12356'
+        result = http.find_vendor_by_user_agent(user_agent)
+
+        assert_that(result, equal_to(expected_result))
