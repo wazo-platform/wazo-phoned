5e3d5978b2c1f70a7d409552543a1998b8ccb874
Alexandre Fournier <afournier@wazo.io>
2019-11-08 14:48:03 -0500

    tests: setup auth users before running

--- integration_tests/suite/helpers/base.py
+++ integration_tests/suite/helpers/base.py
@@ -6,7 +6,10 @@ import os
 
 import requests
 
+from wazo_auth_client import Client as AuthClient
+from xivo.config_helper import parse_config_file
 from xivo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
+from xivo_test_helpers.auth import AuthClient as MockAuthClient, MockCredentials, MockUserToken
 from xivo_test_helpers.wait_strategy import NoWaitStrategy
 
 logger = logging.getLogger(__name__)
@@ -19,7 +22,18 @@ CA_CERT = os.path.join(ASSETS_ROOT, 'ssl', 'server.crt')
 DEFAULT_PROFILE = 'default_phone'
 VALID_TERM = 'toto'
 VALID_VENDOR = 'cisco'
-VALID_XIVO_USER_UUID = 'uuid'
+
+VALID_TOKEN = 'valid-token'
+
+WAZO_UUID = 'de8f8614-9f42-4fb6-be5a-f9a8d4f8674b'
+
+MASTER_TOKEN = '8843a6b5-75e9-4472-8c8c-4a5c605bd47c'
+MASTER_USER_UUID = '5f243438-a429-46a8-a992-baed872081e0'
+MASTER_TENANT = '16d4c16b-eb84-4a09-a3a9-c8cfdf67c8f1'
+
+USERS_TENANT = '938bb555-812d-48c7-9dcb-81f30bd99679'
+USER_1_UUID = 'f2ad2e07-11df-4986-9ecc-c9a29a349725'
+USER_1_TOKEN = '0d74df1c-e59c-4c7a-9f4f-db8ea1f6cd94'
 
 
 class BasePhonedIntegrationTest(AssetLaunchingTestCase):
@@ -32,6 +46,62 @@ class BasePhonedIntegrationTest(AssetLaunchingTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.wait_strategy.wait(cls)
+        if cls.asset != 'no_auth_server':
+            cls.configure_wazo_auth()
+
+    @classmethod
+    def make_auth(cls):
+        return AuthClient('localhost', cls.service_port(9497, 'auth'), verify_certificate=False)
+
+    @classmethod
+    def make_mock_auth(cls):
+        return MockAuthClient('localhost', cls.service_port(9497, 'auth'))
+
+    @classmethod
+    def configure_wazo_auth(cls):
+        key_file = parse_config_file(
+            os.path.join(cls.assets_root, 'auth_keys', 'wazo-phoned-key.yml')
+        )
+        mock_auth = cls.make_mock_auth()
+        mock_auth.set_valid_credentials(
+            MockCredentials(key_file['service_id'], key_file['service_key']),
+            MASTER_TOKEN,
+        )
+        mock_auth.set_token(
+            MockUserToken(
+                MASTER_TOKEN,
+                MASTER_USER_UUID,
+                WAZO_UUID,
+                {'tenant_uuid': MASTER_TENANT, 'uuid': MASTER_USER_UUID},
+            )
+        )
+        mock_auth.set_token(
+            MockUserToken(
+                USER_1_TOKEN,
+                USER_1_UUID,
+                WAZO_UUID,
+                {'tenant_uuid': USERS_TENANT, 'uuid': USER_1_UUID},
+            )
+        )
+
+        mock_auth.set_tenants(
+            {
+                'uuid': MASTER_TENANT,
+                'name': 'phoned-tests-master',
+                'parent_uuid': MASTER_TENANT,
+            },
+            {
+                'uuid': USERS_TENANT,
+                'name': 'phoned-tests-users',
+                'parent_uuid': MASTER_TENANT,
+            },
+        )
+
+        auth = cls.make_auth()
+        auth.users.new(
+            uuid=USER_1_UUID,
+            tenant_uuid=USERS_TENANT,
+        )
 
     @classmethod
     def get_status_result(self):
