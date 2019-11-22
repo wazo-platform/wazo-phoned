5dead3c09aac5559b2606caef3395ae62fda5a60
Alexandre Fournier <afournier@wazo.io>
2019-11-22 14:59:05 -0500

    thomson: migrate from dird

--- wazo_phoned/plugins/thomson/http.py
+++ wazo_phoned/plugins/thomson/http.py
@@ -1,8 +1,10 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_phoned.plugin_helpers.proxy.http import ProxyLookup
+from wazo_phoned.plugin_helpers.client.http import ClientLookup
 
 
-class Lookup(ProxyLookup):
-    pass
+class Lookup(ClientLookup):
+    MAX_ITEM_PER_PAGE = 8
+    content_type = 'text/xml; charset=utf-8'
+    template = 'thomson_results.jinja'
