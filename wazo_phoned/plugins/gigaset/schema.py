ba23a47dc4b9beb6559621b4f0656c5f1cba9959
Pascal Cadotte Michaud <pcm@wazo.io>
2024-12-31 14:45:32 -0500

    schemas: replace missing and default
    missing and default are deprecated and have been replaced by
    load_default and dump_default respectively

--- wazo_phoned/plugins/gigaset/schema.py
+++ wazo_phoned/plugins/gigaset/schema.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo.mallow import fields
@@ -6,6 +6,6 @@ from xivo.mallow_helpers import Schema
 
 
 class LookupGigasetSchema(Schema):
-    set_first = fields.String(attribute='term', missing='')
-    count = fields.Integer(attribute='limit', missing=None)
-    first = fields.Integer(attribute='offset', missing=1)
+    set_first = fields.String(attribute='term', load_default='')
+    count = fields.Integer(attribute='limit', load_default=None)
+    first = fields.Integer(attribute='offset', load_default=1)
